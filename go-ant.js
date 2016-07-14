3c03ec9b29b3fdd95693da3d1b7baef1f479bd9d
Jooyung Han <jooyung.han@lge.com>
2016-07-14 11:59:01 +0900

    Extract foldM from channel recv loop

--- go-ant.js
+++ go-ant.js
@@ -1,9 +1,9 @@
-const {run, read, writeln, go, chan, send, recv, close, done} = require('./go')
+const {run, writeln, go, chan, send, recv, close, done} = require('./go')
 const {forever} = require('./monad')
 
 // Look-and-say sequence
 // in CSP way
- 
+
 function ant(n) {
   if (n == 0) return init()
   else return chan()
@@ -14,27 +14,48 @@ function ant(n) {
 
 function init() {
   return chan()
-    .flatMap(c => 
+    .flatMap(c =>
       go(send(c, 1).flatMap(() => close(c)))
-      .flatMap(() => done(c)))
+        .flatMap(() => done(c)))
+}
+
+// (a -> b -> m a) -> a -> [b] -> m a
+function foldM(f, z, ch) {
+  return recv(ch).flatMap(v => (v === 0) ? done(z) : f(z, v).flatMap(zz => foldM(f, zz, ch)))
+}
+
+function send2(ch, a, b) {
+  return send(ch, a).flatMap(() => send(ch, b))
 }
 
 function next(i, o) {
-  function loop(prev, count) {
-    return recv(i).flatMap(c => {
-      if (c === 0) {
-        return send(o, count).flatMap(() => send(o, prev))
-      } else if (c === prev) {
-        return loop(prev, count + 1)
-      } else {
-        return send(o, count).flatMap(() => send(o, prev)).flatMap(() => loop(c, 1))
-      }
-    })
+  return recv(i)
+    .flatMap(prev => foldM(f, {item: prev, count: 1}, i))
+    .flatMap(({item, count}) => send2(o, count, item))
+    .flatMap(() => close(o))
+
+  function f({item,count}, c) {
+    return item === c ? done({item, count: count + 1})
+      : send2(o, count, item).flatMap(() => done({item: c, count: 1}))
   }
-  return recv(i).flatMap(prev => {
-    return loop(prev, 1)
-  }).flatMap(() => close(o))
 }
+
+// function next(i, o) {
+//   function loop(prev, count) {
+//     return recv(i).flatMap(c => {
+//       if (c === 0) {
+//         return send(o, count).flatMap(() => send(o, prev))
+//       } else if (c === prev) {
+//         return loop(prev, count + 1)
+//       } else {
+//         return send(o, count).flatMap(() => send(o, prev)).flatMap(() => loop(c, 1))
+//       }
+//     })
+//   }
+//   return recv(i).flatMap(prev => {
+//     return loop(prev, 1)
+//   }).flatMap(() => close(o))
+// }
 
 function each(ch, f) {
   return recv(ch).flatMap(v => v === 0 ? done() : f(v).flatMap(() => each(ch, f)))
@@ -42,8 +63,8 @@ function each(ch, f) {
 
 function nth(ch, n) {
   return n === 0 ? recv(ch)
-    : recv(ch).flatMap(() => nth(ch, n-1))
+    : recv(ch).flatMap(() => nth(ch, n - 1))
 }
 
-// node --max-old-space-size=4096 go-ant.js
-run(ant(1000000).flatMap(ch => nth(ch, 1000000)).flatMap(writeln))
+// $ node --max-old-space-size=4096 go-ant.js
+run(ant(100).flatMap(ch => nth(ch, 100)).flatMap(writeln))
